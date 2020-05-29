5293f18927d84399107108152ad7922c8514d8d9
Takuma_Kitahara <takumakitahara.jp@gmail.com>
2020-05-29 22:37:24 +0900

    #1 日付

--- baba.js
+++ baba.js
@@ -79,22 +79,23 @@ document.getElementById('search').addEventListener('click', () => {
   //2だった場合は正常処理
   if(displayCounter == 2) { 
     //日付
-    document.getElementById('trendDay').textContent = displayDay;
+    
     callSpread(function(keibaDataArr) {
-      keibaDataArr.forEach((dayValue, index) => {
-        if(cutDate.slice(-6) == dayValue[0]) {
-          console.log(keibaDataArr[index][1]);
-          console.log(keibaDataArr[index][2]);
-          console.log(keibaDataArr[index][3]);
-          console.log(keibaDataArr[index][4]);
-          console.log(keibaDataArr[index][5]);
-          console.log(keibaDataArr[index][6]);
+      keibaDataArr.forEach((dayLocatValue, index) => {
+        let resultHtml = '<ul>';
+        if(cutDate.slice(-6) == dayLocatValue[0] && displayLocation == dayLocatValue[1]) {
+          document.getElementById('trendDay').textContent = displayDay;
+          document.getElementById('trendField').textContent = displayLocation;
+          resultHtml += '<li>' + keibaDataArr[index][2] + 'R' + '</li>';
+          resultHtml += '<li>' + keibaDataArr[index][3] + '</li>';
+          resultHtml += '<li>' + keibaDataArr[index][4] + '</li>';
+          resultHtml += '<li>' + keibaDataArr[index][5] + '</li>';
+          resultHtml += '<li>' + keibaDataArr[index][6] + '</li>';
         }
+        resultHtml += '</ul>';
+        document.getElementById('resultHtml').innerHTML = resultHtml;
       });
     });
-
-    //開催場
-    document.getElementById('trendField').textContent = displayLocation;   
   } else {
     displayCounter = 0;
   }
