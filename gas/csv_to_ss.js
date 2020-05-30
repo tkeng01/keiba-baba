b8ce08a0e531d0e672a1596fd48e2da51df3fd96
ry0-cha-ng <designsyobo@gmail.com>
2020-05-30 20:19:23 +0900

    スクリプトを機能別で分離

---
+++ gas/csv_to_ss.js
@@ -0,0 +1,19 @@
+/*
+function ss_to_json(){
+// 行ごとのヘッダー（データ名）を定義
+    var year_col = "A";
+    var month_col = "B";
+    var day_col = "C";
+    var location_col = "D";
+    var race_col = "E";
+    var class_col = "F";
+    var field_col = "G";
+    var distance_col = "H";
+    var condition_col = "I";
+    var place_col = "J";
+    var popular_col = "K";
+    var corner4_col = "L";
+    var PCI_col = "M";
+}
+*/
+
--- gas/ss_to_json.js
+++ gas/ss_to_json.js
@@ -1,36 +1,11 @@
-/*
-function ss_to_json(){
-// 行ごとのヘッダー（データ名）を定義
-    var year_col = "A";
-    var month_col = "B";
-    var day_col = "C";
-    var location_col = "D";
-    var race_col = "E";
-    var class_col = "F";
-    var field_col = "G";
-    var distance_col = "H";
-    var condition_col = "I";
-    var place_col = "J";
-    var popular_col = "K";
-    var corner4_col = "L";
-    var PCI_col = "M";
-}
-*/
-
 function getData(id, sheetName) {
     var sheet = SpreadsheetApp.openById(id).getSheetByName(sheetName);
     var rows = sheet.getDataRange().getValues();
-
-    /*ここでヘッダーを取得する
-        spliceメソッド
-        Arrayオブジェクト.splice(配列番号, 削除する要素数, 追加する要素1, 追加する要素2, …)
-            ・指定の配列番号の位置について
-            ・削除する要素数分の要素を削除しつつ
-            ・追加する要素1,追加する要素2,…を追加する（省略可能）
-    */
     var keys = rows.splice(0, 1)[0];
     return rows.map(function(row) {
+        // 中身が空のオブジェクトobjを作成
         var obj = {}
+        // rowsで取得したss内のデータを配列化してobjに格納する
         row.map(function(item, index) {
         obj[keys[index]] = item;
     });
@@ -40,7 +15,19 @@ function getData(id, sheetName) {
 
 function doGet(request) {
     var func = 'jsondata';
-    var data = getData('https://docs.google.com/spreadsheets/d/1kxx6dnlY0CN-KGx92Rf-nYVskcG9udSGRLb8Xj76ezw/edit#gid=0', 'Sheet1');
+    var ssID = '1kxx6dnlY0CN-KGx92Rf-nYVskcG9udSGRLb8Xj76ezw'
+    var ssSheetName = 'Sheet1'
+    var data = getData( ssID , ssSheetName );
     return ContentService.createTextOutput(func + '(' + JSON.stringify(data, null, 2) + ')')
     .setMimeType(ContentService.MimeType.JAVASCRIPT);
 }
+/*
+  spliceメソッド
+    Arrayオブジェクト.splice(配列番号, 削除する要素数, 追加する要素1, 追加する要素2, …)
+    ・指定の配列番号の位置について
+    ・削除する要素数分の要素を削除しつつ
+    ・追加する要素1,追加する要素2,…を追加する（省略可能）
+  
+  mapメソッド
+    処理をして新しい配列を作る
+*/
